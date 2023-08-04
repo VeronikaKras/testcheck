39c2498e94f9bae43a7f8bc30d7479c3f9810790
Veronika <nikakras.dance@gmail.com>
2023-08-04 15:07:06 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,16 +3,26 @@
 const startBtn = document.querySelector(".start");
 const stopBtn = document.querySelector(".stop");
 let timerId = null;
-
+const body = document.querySelector('body')
   
+
+
+
 startBtn.addEventListener("click", () => {
   timerId = setInterval(() => {
-    getRandomHexColor();
+    body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 });
-
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    
+});
+
+if (getRandomHexColor()) {
+   startBtn.setAttribute('disabled', true)
+  }
